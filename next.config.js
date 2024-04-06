9fce9a1d325e1be6a1b9a40e28526c2ced3632b2
DiGiV3rse <kimhabork@icloud.com>
2024-04-07 02:46:05 +0700

    update

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+module.exports = {
+	experimental: {
+		serverActions: {
+			allowedOrigins: ["app.localhost:3000"],
+		},
+	},
+	images: {
+		remotePatterns: [
+			{ hostname: "public.blob.vercel-storage.com" },
+			{ hostname: "res.cloudinary.com" },
+			{ hostname: "abs.twimg.com" },
+			{ hostname: "pbs.twimg.com" },
+			{ hostname: "avatar.vercel.sh" },
+			{ hostname: "avatars.githubusercontent.com" },
+			{ hostname: "www.google.com" },
+			{ hostname: "flag.vercel.app" },
+			{ hostname: "cloudflare-ipfs.com" },
+			{ hostname: "ipfs.io" },
+			{ hostname: "ipfs.nftstorage.link" },
+			{ hostname: "illustrations.popsy.co" },
+    ]
+	},
+};
---
+++ tailwind.config.js
@@ -0,0 +1,175 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}", // Tremor module
+  ],
+	theme: {
+		extend: {
+			colors: {
+				// light mode
+				tremor: {
+					brand: {
+						faint: "#E0F4FF", // blue-50
+						muted: "#C4E2F3", // blue-200
+						subtle: "#99D7F9", // blue-400
+						DEFAULT: "#5ABEF5", // blue-500
+						emphasis: "#139BE6", // blue-700
+						inverted: "#FFFFFF", // white
+					},
+					background: {
+						muted: "#E5E7EA", // gray-50
+						subtle: "#CFD1D5", // gray-100
+						DEFAULT: "#FFFFFF", // white
+						emphasis: "#40464F", // gray-700
+					},
+					border: {
+						DEFAULT: "#EEEEEE", // gray-200
+					},
+					ring: {
+						DEFAULT: "#D5D8DD", // gray-200
+					},
+					content: {
+						subtle: "#AAAFB7", // gray-400
+						DEFAULT: "#83878F", // gray-500
+						emphasis: "#2F353E", // gray-700
+						strong: "#020305", // gray-900
+						inverted: "#ffffff", // white
+					},
+				},
+				// dark mode
+				"dark-tremor": {
+					brand: {
+						faint: "#0B2529", // custom
+						muted: "#174954", // blue-950
+						subtle: "#1E9FAF", // blue-800
+						DEFAULT: "#3BE2F6", // blue-500
+						emphasis: "#60F0FA", // blue-400
+						inverted: "#031211", // gray-950
+					},
+					background: {
+						muted: "#13292B", // custom
+						subtle: "#1F3137", // gray-800
+						DEFAULT: "#112327", // gray-900
+						emphasis: "#D1DADB", // gray-300
+					},
+					border: {
+						DEFAULT: "#1F3437", // gray-800
+					},
+					ring: {
+						DEFAULT: "#1f2937", // gray-800
+					},
+					content: {
+						subtle: "#4B6163", // gray-600
+						DEFAULT: "#6B7D80", // gray-600
+						emphasis: "#E5EBEB", // gray-200
+						strong: "#F9FBFB", // gray-50
+						inverted: "#000000", // black
+					},
+				},
+			},
+			boxShadow: {
+				// light
+				"tremor-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
+				"tremor-card": "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
+				"tremor-dropdown": "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
+				// dark
+				"dark-tremor-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
+				"dark-tremor-card": "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
+				"dark-tremor-dropdown": "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
+			},
+			borderRadius: {
+				"tremor-small": "0.375rem",
+				"tremor-default": "0.5rem",
+				"tremor-full": "9999px",
+			},
+			fontSize: {
+				"tremor-label": ["0.75rem"],
+				"tremor-default": ["0.875rem", { lineHeight: "1.25rem" }],
+				"tremor-title": ["1.125rem", { lineHeight: "1.75rem" }],
+				"tremor-metric": ["1.875rem", { lineHeight: "2.25rem" }],
+			},
+			width: {
+				1536: "1536px",
+			},
+			height: {
+				150: "37.5rem",
+			},
+			margin: {
+				30: "7.5rem",
+			},
+			fontFamily: {
+				default: ["var(--font-inter)", ...fontFamily.sans],
+				cal: ["var(--font-cal)", ...fontFamily.sans],
+				title: ["var(--font-title)", ...fontFamily.sans],
+				mono: ["Consolas", ...fontFamily.mono],
+			},
+			typography: {
+				DEFAULT: {
+					css: {
+						h1: {
+							fontFamily: "Cal Sans",
+						},
+						h2: {
+							fontFamily: "Cal Sans",
+						},
+						h3: {
+							fontFamily: "Cal Sans",
+						},
+						"blockquote p:first-of-type::before": { content: "none" },
+						"blockquote p:first-of-type::after": { content: "none" },
+					},
+				},
+			},
+			keyframes: {
+				wiggle: {
+					"0%, 100%": {
+						transform: "translateX(0%)",
+						transformOrigin: "50% 50%",
+					},
+					"15%": { transform: "translateX(-6px) rotate(-6deg)" },
+					"30%": { transform: "translateX(9px) rotate(6deg)" },
+					"45%": { transform: "translateX(-9px) rotate(-3.6deg)" },
+					"60%": { transform: "translateX(3px) rotate(2.4deg)" },
+					"75%": { transform: "translateX(-2px) rotate(-1.2deg)" },
+				},
+			},
+			animation: {
+				wiggle: "wiggle 0.8s both",
+			},
+		},
+	},
+	safelist: [
+		{
+			pattern: /^(bg-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+			variants: ["hover", "ui-selected"],
+    },
+		{
+			pattern: /^(text-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+			variants: ["hover", "ui-selected"],
+    },
+		{
+			pattern: /^(border-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+			variants: ["hover", "ui-selected"],
+    },
+		{
+			pattern: /^(ring-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+    },
+		{
+			pattern: /^(stroke-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+    },
+		{
+			pattern: /^(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+    },
+  ],
+	plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("tailwindcss-animate"),
+  ],
+};
